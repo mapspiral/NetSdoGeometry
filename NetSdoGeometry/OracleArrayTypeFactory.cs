f4a261014218e8a8069202723ce3649e3e994e11
Robert van Drunen <r.j.m.vandrunen@gmail.com>
2013-02-14 16:34:32 +0100

    StyleCop formatting

--- NetSdoGeometry/OracleArrayTypeFactory.cs
+++ NetSdoGeometry/OracleArrayTypeFactory.cs
@@ -1,9 +1,9 @@
-using System;
-using Oracle.DataAccess.Types;
-using Oracle.DataAccess.Client;
-
 namespace NetSdoGeometry
 {
+    using System;
+    using Oracle.DataAccess.Client;
+    using Oracle.DataAccess.Types;
+
     public abstract class OracleArrayTypeFactoryBase<T> : IOracleArrayTypeFactory
     {
         public Array CreateArray(int numElems)
