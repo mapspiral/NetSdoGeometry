1fa033092ddbfee8de6366d4ae8efa8d10d2862f
Robert van Drunen <r.j.m.vandrunen@gmail.com>
2013-02-14 16:29:33 +0100

    Renamed sdogeometry to SdoGeometry

--- NetSdoGeometry/SdoGeometry.cs
+++ NetSdoGeometry/SdoGeometry.cs
@@ -21,7 +21,7 @@ namespace NetSdoGeometry
     }
     [Serializable]
     [OracleCustomTypeMappingAttribute("MDSYS.SDO_GEOMETRY")]
-    public class sdogeometry : OracleCustomTypeBase<sdogeometry>
+    public class SdoGeometry : OracleCustomTypeBase<SdoGeometry>
     {
         private enum OracleObjectColumns { SDO_GTYPE, SDO_SRID, SDO_POINT, SDO_ELEM_INFO, SDO_ORDINATES }
 
